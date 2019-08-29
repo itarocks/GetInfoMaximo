0a77792f7705e2149beff63de0adbdf92b81b016
Itamar Rocha <itamarrocha@MacBook-Pro-de-Itamar.local>
2019-08-28 21:58:36 -0300

    first commit

---
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -0,0 +1,26 @@
+package br.com.casadocodigo.loja.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {AppWebConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		
+		return new String[] {"/"};
+		
+	}
+
+}
