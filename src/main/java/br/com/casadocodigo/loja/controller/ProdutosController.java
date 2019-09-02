75f43ae01e8d80e751d4c6914865723ecbeba772
Itamar Rocha <itamarrochaa@yahoo.com>
2019-09-02 06:55:03 -0300

    incluindo as classes e configurações do JPA

--- src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controller/ProdutosController.java
@@ -1,12 +1,17 @@
 package br.com.casadocodigo.loja.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.casadocodigo.loja.daos.ProdutoDao;
 import br.com.casadocodigo.loja.model.Produto;
 
 @Controller
 public class ProdutosController {
+	
+	@Autowired
+	public ProdutoDao produtoDao;
 	
 	@RequestMapping("produtos/form")
 	public String form() {
@@ -17,7 +22,7 @@ public class ProdutosController {
 	@RequestMapping("/produtos")
 	public String grava(Produto produto) {
 		System.out.println(produto);
-
+		produtoDao.gravar(produto);
 		return "produtos/ok";
 	}
 
--- src/main/java/br/com/casadocodigo/loja/daos/ProdutoDao.java
+++ src/main/java/br/com/casadocodigo/loja/daos/ProdutoDao.java
@@ -1,5 +1,24 @@
 package br.com.casadocodigo.loja.daos;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.casadocodigo.loja.model.Produto;
+
+@Repository
+@Transactional
 public class ProdutoDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void gravar(Produto produto) {
+
+		manager.persist(produto);
+
+	}
 
 }
