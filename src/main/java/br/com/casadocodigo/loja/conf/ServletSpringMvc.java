4bc6a3b1f81ea6a1002d9d60571a56c9e8e02b0f
Itamar Rocha <itamarrochaa@yahoo.com>
2019-09-02 22:26:17 -0300

    configurando o encoding das requisições

--- src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
+++ src/main/java/br/com/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.conf;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletInitializer{
@@ -20,6 +23,18 @@ public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletI
 	protected String[] getServletMappings() {
 		
 		return new String[] {"/"};
+		
+	}
+	
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		// TODO Auto-generated method stub
+		
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		
+		return new Filter[] {encodingFilter};
 		
 	}
 
